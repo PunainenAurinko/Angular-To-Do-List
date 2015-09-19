53ecf26857b86a258bb700f36376d5bbac5d4383
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2015-09-18 22:31:26 -0400

    completed app w/ checkboxes & delete done tasks btn

--- js/main.js
+++ js/main.js
@@ -8,18 +8,14 @@ var todoApp = angular.module('todoList', []);
                 {'title':'Code some more', 'done':false}
             ];
             
-            $scope.addTask = function addTask() {
+            $scope.addTask = function() {
                 $scope.tasks.push({'title':$scope.newTask, 'done':false});
-                $scope.data.task = '';
-            }
-                        
-            $scope.markAsDone = function markAsDone() {
-                return $scope.tasks.done;
+                $scope.newTask = '';
             }
             
-            $scope.deleteTask = function deleteTask() {
-                $scope.tasks.splice($scope.data.task);
+            $scope.clearDoneTasks = function() {
+                $scope.tasks = $scope.tasks.filter(function(item){
+                    return !item.done;
+                })
             }
-            
-            $scope.clearCompleted = function(){}
         }]);

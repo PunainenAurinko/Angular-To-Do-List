4129b18392c44c87e493f785ff28ff6b0c0c9cc4
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2015-09-18 21:34:15 -0400

    fixed task adding functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var todoApp = angular.module('todoList', []);
         todoApp.controller('todoListController', ['$scope', function ($scope) {
             $scope.name = 'to-do list';
-            $scope.data = {};
+            
             $scope.tasks = [
                 {'title':'Learn some stuff', 'done':false}, 
                 {'title':'Code some stuff', 'done':false},
                 {'title':'Code some more', 'done':false}
             ];
             
-            $scope.addTask = function submitTask() {
-                $scope.tasks.push($scope.data.task);
+            $scope.addTask = function addTask() {
+                $scope.tasks.push({'title':$scope.newTask, 'done':false});
                 $scope.data.task = '';
             }
                         

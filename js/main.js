872e491b125ca14644b29b72ed281b30b4f40bab
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2015-09-18 23:15:03 -0400

    final polishes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
 var todoApp = angular.module('todoList', []);
-        todoApp.controller('todoListController', ['$scope', function ($scope) {
-            $scope.name = 'to-do list';
-            
-            $scope.tasks = [
-                {'title':'Learn some stuff', 'done':false}, 
-                {'title':'Code some stuff', 'done':false},
-                {'title':'Code some more', 'done':false}
+todoApp.controller('todoListController', ['$scope', function ($scope) {
+    
+    $scope.name = 'to-do list';
+
+    $scope.tasks = [
+        {
+            'title': 'Learn some stuff',
+            'done': false
+        },
+        {
+            'title': 'Code some stuff',
+            'done': false
+        },
+        {
+            'title': 'Code some more',
+            'done': false
+        }
             ];
-            
-            $scope.addTask = function() {
-                $scope.tasks.push({'title':$scope.newTask, 'done':false});
-                $scope.newTask = '';
-            }
-            
-            $scope.clearDoneTasks = function() {
-                $scope.tasks = $scope.tasks.filter(function(item){
-                    return !item.done;
-                })
-            }
-        }]);
+
+    $scope.addTask = function () {
+        
+        $scope.tasks.push({
+            'title': $scope.newTask,
+            'done': false
+        });
+        
+        $scope.newTask = '';
+    }
+
+    $scope.clearDoneTasks = function () {
+        $scope.tasks = $scope.tasks.filter(function (item) {
+            return !item.done;
+        })
+    }
+
+}]);
